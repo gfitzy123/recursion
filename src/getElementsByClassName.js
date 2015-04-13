7fb06483ea2e4146195b268a4bb2b331eda2ae44
Garrett Fitzgerald <gfitzgerald123@gmail.com>
2015-04-13 18:07:46 -0500

    Completed getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
-};
+var getElementsByClassName = function(className) {
+
+  var result = [];
+
+  var recursiveFunction = function (node) {
+
+// If the node contains classes
+    if (node.classList) {
+      if (_.contains(node.classList, className)) {
+//push the nodes that contain className to the result array
+        result.push(node);
+      }
+    }
+// If the node contains childnodes
+    if (node.childNodes.length > 0) {
+      for (var j = 0; j < node.childNodes.length; j++) {
+// We must check each node for childNodes, and each chlildNode's classes (and then those classes that match className)
+// And then push those matching nodes to the result array.
+
+        recursiveFunction(node.childNodes[j]);
+      }
+    }
+  }
+  
+  recursiveFunction(document.body);
+  return result;
+
+}
